bba98d1322d411577fdc4fa2a2a46abf27510e47
savicaleksa <aleksa.savic91@gmail.com>
2024-06-21 22:31:10 +0200

    add package visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react"
 import path from "path"
+import { visualizer } from "rollup-plugin-visualizer"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
